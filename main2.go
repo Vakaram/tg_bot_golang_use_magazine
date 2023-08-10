30b97fd4dba6d5f4a4d79c627ba46f93d29e0af8
vladimirmakarov <Vakaramm@gmail.com>
2023-08-10 07:54:47 -0700

    добавил возможность получать информацию из базы данных и сразу в кнопки их переводить

--- main2.go
+++ main2.go
@@ -1,36 +1,3 @@
-//package main
-//
-//import (
-//	"log"
-//
-//	"github.com/go-telegram-bot-api/telegram-bot-api/v5"
-//)
-//
-//func main() {
-//	bot, err := tgbotapi.NewBotAPI("5975063523:AAFagQJfXf3z-zgA0JjHPusoGhjjXIYOyEI")
-//	if err != nil {
-//		log.Panic(err)
-//	}
-//
-//	log.Printf("Authorized on account %s", bot.Self.UserName)
-//
-//	u := tgbotapi.NewUpdate(0)
-//	u.Timeout = 60
-//
-//	updates := bot.GetUpdatesChan(u)
-//
-//	for update := range updates {
-//		if update.Message != nil { // If we got a message
-//			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-//
-//			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-//			msg.ReplyToMessageID = update.Message.MessageID
-//
-//			bot.Send(msg)
-//		}
-//	}
-//}
-
 package main
 
 import (
@@ -39,53 +6,85 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	_ "github.com/mattn/go-sqlite3"
 	"log"
-	"strconv"
 )
+
+func createButton(arrayButton []string) []tgbotapi.KeyboardButton { // получает массив значений и выдает кнопки меню для тг
+	var itog []tgbotapi.KeyboardButton
+	//a := tgbotapi.NewKeyboardButton("44") /// я тут разложил все а потом цикл понял как писать =)
+	//b := tgbotapi.NewKeyboardButton("55")
+	//c := tgbotapi.NewKeyboardButton("66")
+	//itog = append(itog, a)
+	//itog = append(itog, b)
+	//itog = append(itog, c)
+	////
+	//lenarray := len(arrayButton)
+	//for i := 0; i < lenarray; i++ {
+	//	itog = append(itog, tgbotapi.NewKeyboardButton(strconv.Itoa(arrayButton[i])))
+	//}
+
+	for _, v := range arrayButton {
+		itog = append(itog, tgbotapi.NewKeyboardButton(v))
+	}
+
+	return itog
+}
 
 // create db
 
 func createTable() {
 	database, _ := sql.Open("sqlite3", "./info.db")
+	defer database.Close()
+
 	statematn, _ := database.Prepare("CREATE TABLE IF NOT EXISTS bani(id INTEGER PRIMARY KEY ,buttun TEXT, description TEXT, photo TEXT)")
 	statematn.Exec()
 	statematn, _ = database.Prepare("INSERT INTO bani (buttun, description, photo) VALUES (?,?,?)") // добавляет значения
 	statematn.Exec("First", "Second", "lol")
 	rows, _ := database.Query("SELECT id,buttun,photo FROM bani") // запрос делаем
+
 	var id int
 	var but string
 	var phot string
 	for rows.Next() {
 		rows.Scan(&id, &but, &phot)
-		fmt.Printf("%d: %s ,%s \n", id, but, phot)
+		fmt.Printf("%d: %s x,%s это из бд\n ", id, but, phot)
 	}
-
 }
 
-//	type testbut struct {
-//		name
-//		namebutton string
-//	}
-//
-//	func NewReplyKeyboard(rows ...[]KeyboardButton) ReplyKeyboardMarkup {
-//		var keyboard [][]KeyboardButton
-//
-//		keyboard = append(keyboard, rows...)
-//
-//		return ReplyKeyboardMarkup{
-//			ResizeKeyboard: true,
-//			Keyboard:       keyboard,
-//		}
-//	}
+func giveButtonInBase() []string {
+	var givButton []string // функция для получения кнопок которые отправятся в бд
+
+	database, _ := sql.Open("sqlite3", "./info.db")
+	rows, _ := database.Query("SELECT buttun FROM bani") // запрос делаем
+	defer database.Close()
+
+	var but string
+	for rows.Next() {
+		rows.Scan(&but)
+		givButton = append(givButton, but)
+		//fmt.Printf("%d: %s ,%s \n", id, but, phot)
+	}
+	return givButton
+}
 
 func main() {
-	//fmt.Printf("%T :переменная button\n", button)
-	fmt.Printf("%T :переменная тест2\n", test2)
 
-	fmt.Printf("%T :переменная numeric02\n", numeric02)
-	fmt.Printf("%T :переменная numeric01\n", numeric01)
-	fmt.Printf("%T :переменная numericKeyboard\n", numericKeyboard)
-	//createTable() // вызвали и создали базу
-	bot, err := tgbotapi.NewBotAPI("")
+	buttonBase := tgbotapi.NewReplyKeyboard(createButton(giveButtonInBase()))
+	//fmt.Printf("%T :переменная button\n", button)
+	//fmt.Printf("%T :переменная тест2\n", test2)
+	//
+	//fmt.Printf("%T :переменная numeric02\n", numeric02)
+	//fmt.Printf("%T :переменная numeric01\n", numeric01)
+	//fmt.Printf("%T :переменная numericKeyboard\n", numericKeyboard)
+	//var rows *sql.Rows
+	createTable() // вызвали и создали базу
+	////здесь получаем из крейт тейбл наши данные и их распаршиваем в нашу функцию а потом передаем инфо в кнопки =)
+	//var id int
+	//for rows.Next() {
+	//	rows.Scan(&id)
+	//	fmt.Printf("%d:\n", id)
+	//}
+
+	bot, err := tgbotapi.NewBotAPI("5975063523:AAFagQJfXf3z-zgA0JjHPusoGhjjXIYOyEI")
 	if err != nil {
 		log.Panic(err)
 	}
@@ -108,7 +107,7 @@ func main() {
 
 		switch update.Message.Text {
 		case "open":
-			msg.ReplyMarkup = test2
+			msg.ReplyMarkup = buttonBase
 		case "close":
 			msg.ReplyMarkup = tgbotapi.NewRemoveKeyboard(true)
 		}
@@ -119,44 +118,9 @@ func main() {
 	}
 }
 
-func createButton() []tgbotapi.KeyboardButton {
-	var itog []tgbotapi.KeyboardButton
-	//a := tgbotapi.NewKeyboardButton("44")
-	//b := tgbotapi.NewKeyboardButton("55")
-	//c := tgbotapi.NewKeyboardButton("66")
-	//itog = append(itog, a)
-	//itog = append(itog, b)
-	//itog = append(itog, c)
-
-	for i := 0; i < 5; i++ {
-		itog = append(itog, tgbotapi.NewKeyboardButton(strconv.Itoa(i)))
-	}
-
-	return itog
-}
-
 //
 //var button = createButton()
 //
 //var value1 = []tgbotapi.KeyboardButton{}
 
 //var button = []tgbotapi.KeyboardButton{tgbotapi.NewKeyboardButton("11"), tgbotapi.NewKeyboardButton("22")}
-
-var test2 = tgbotapi.NewReplyKeyboard(createButton())
-
-var numeric02 = tgbotapi.NewKeyboardButton("1")
-var numeric01 = tgbotapi.NewKeyboardButtonRow(
-	numeric02,
-)
-var numericKeyboard = tgbotapi.NewReplyKeyboard(
-	tgbotapi.NewKeyboardButtonRow(
-		tgbotapi.NewKeyboardButton("1"),
-		tgbotapi.NewKeyboardButton("2"),
-		tgbotapi.NewKeyboardButton("3"),
-	),
-	tgbotapi.NewKeyboardButtonRow(
-		tgbotapi.NewKeyboardButton("4"),
-		tgbotapi.NewKeyboardButton("5"),
-		tgbotapi.NewKeyboardButton("6"),
-	),
-)
